1d16f8bf17423f3110ea619ee0cd15d069e5861f
Nilkanth There <nmtherethere@gmail.com>
2025-09-09 10:29:15 +0530

    Main Method file added

---
+++ src/main/java/com/example/newcruddemo/NewcruddemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.newcruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewcruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewcruddemoApplication.class, args);
+	}
+
+}
